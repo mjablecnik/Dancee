628c2b5ca2eac91200ca716780c2a617b139636e
Martin Jablečník <martin.jablecnik@email.cz>
2025-02-26 12:23:26 +0100

    refactor(dancee_app): remove products feature

--- dancee_app/lib/global.dart
+++ dancee_app/lib/global.dart
@@ -1,7 +1,6 @@
 import 'package:dancee_app/features/events/event_list_cubit.dart';
 import 'package:dancee_app/features/auth/auth_repository.dart';
 import 'package:dancee_app/features/events/event_repository.dart';
-import 'package:dancee_app/features/products/product_repository.dart';
 import 'package:dancee_app/features/app/user_repository.dart';
 import 'package:vader_app/vader_app.dart';
 
@@ -11,7 +10,6 @@ setupInjector({required HttpClient httpClient, required StorageClient storageCli
     ..addInstance(storageClient)
     ..addSingleton(AuthRepository.new)
     ..addSingleton(UserRepository.new)
-    ..addSingleton(ProductRepository.new)
     ..addSingleton(EventRepository.new)
     ..addSingleton(EventListCubit.new)
     ..commit();
