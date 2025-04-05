ff27e03719eeea3b63a4358d62f2b2adf6e5bae7
Martin Jablečník <martin.jablecnik@email.cz>
2025-04-05 05:52:49 +0200

    refactor(dancee_app): eventListPage

--- dancee_app/lib/features/events/pages/event_list_page.dart
+++ dancee_app/lib/features/events/pages/event_list_page.dart
@@ -1,11 +1,12 @@
+import 'package:dancee_app/entities/event.dart';
 import 'package:dancee_design/dancee_design.dart';
 import 'package:dancee_app/features/events/logic/event_list_cubit.dart';
 import 'package:dancee_app/features/events/logic/event_list_state.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart' hide Chip;
 import 'package:vader_app/vader_app.dart';
 import 'package:dancee_app/i18n/translations.g.dart';
 
-import '../sections/action_section.dart';
 import 'event_detail_page.dart';
 
 part 'event_list_page.g.dart';
@@ -34,36 +35,68 @@ class EventListPage extends StatelessWidget {
               return state.when(
                 loading: () => Text("Eventy se načítají"),
                 failed: (e) => Text("Nastala chyba: $e"),
-                loaded: (events) {
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: events.length,
-                      itemBuilder: (context, index) {
-                        final event = events[index];
-                        return EventCard(
-                          width: double.infinity,
-                          title: event.title,
-                          place: event.venue.name,
-                          since: event.dateTimeRange.start,
-                          until: event.dateTimeRange.end,
-                          style: EventCardStyle(
-                            decoration: BoxDecoration(
-                              color: index % 2 == 0 ? AppColors.white : AppColors.blue200,
-                            ),
-                          ),
-                          tooMuchInfo: event.tags.length >= 3 && event.title.length > 24,
-                          chips: event.tags.map((t) => Chip(text: t)).toList(),
-                          onTap: () async {
-                            //await injector.use<EventRepository>().getAllEvents();
-                            EventDetailRoute(event).push(context);
-                          },
-                        );
-                      },
-                    ),
-                  );
-                },
+                loaded: (events) => Expanded(child: EventList(events: events)),
               );
             },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EventList extends StatelessWidget {
+  const EventList({super.key, required this.events});
+
+  final List<Event> events;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: events.length,
+      itemBuilder: (context, index) {
+        final event = events[index];
+        return EventCard(
+          width: double.infinity,
+          title: event.title,
+          place: event.venue.name,
+          since: event.dateTimeRange.start,
+          until: event.dateTimeRange.end,
+          style: EventCardStyle(decoration: BoxDecoration(color: index % 2 == 0 ? AppColors.white : AppColors.blue200)),
+          tooMuchInfo: event.tags.length >= 3 && event.title.length > 24,
+          chips: event.tags.map((t) => Chip(text: t)).toList(),
+          onTap: () async {
+            //await injector.use<EventRepository>().getAllEvents();
+            EventDetailRoute(event).push(context);
+          },
+        );
+      },
+    );
+  }
+}
+
+class ActionSection extends StatelessWidget {
+  const ActionSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 42,
+      decoration: BoxDecoration(
+        border: Border.symmetric(horizontal: BorderSide(color: AppColors.grey400.withAlpha(25))),
+      ),
+      child: Row(
+        children: [
+          LinkButton(
+            text: "Filtrovat",
+            icon: Icon(CupertinoIcons.slider_horizontal_3, size: 14, color: AppColors.blue900),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          ),
+          Spacer(),
+          LinkButton(
+            text: "Přidat událost",
+            icon: Icon(Icons.add, color: AppColors.blue900, size: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
           ),
         ],
       ),
