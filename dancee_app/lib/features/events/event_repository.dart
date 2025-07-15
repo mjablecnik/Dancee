4f205aa6251429def4c25ea2932fef6d4897ff36
Martin Jablečník <martin.jablecnik@email.cz>
2025-07-15 13:14:32 +0200

    refactor: improve event repository to handle failed requests with exponential backoff-like retry

--- dancee_app/lib/features/events/event_repository.dart
+++ dancee_app/lib/features/events/event_repository.dart
@@ -3,17 +3,26 @@ import 'package:dancee_shared/entities.dart';
 import 'package:dio/dio.dart';
 
 class EventRepository {
-  Future<List<Event>> getEvents() async {
-    final Dio _dio = Dio(
-      BaseOptions(
-        baseUrl: AppConfig().apiUrl,
-        connectTimeout: const Duration(seconds: 5),
-        headers: {'Content-Type': 'application/json'},
-      ),
-    );
-    final response = await _dio.get('/event/list');
-    return [
-      ...response.data["data"].map((e) => Event.fromJson(e)),
-    ];
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: AppConfig().apiUrl,
+      connectTimeout: const Duration(seconds: 5),
+      headers: {'Content-Type': 'application/json'},
+    ),
+  );
+
+  Future<List<Event>> getEvents({attempts = 3}) async {
+    try {
+      final response = await _dio.get('/event/list');
+      return [
+        ...response.data["data"].map((e) => Event.fromJson(e)),
+      ];
+    } catch (e) {
+      if (attempts > 0) {
+        return getEvents(attempts: --attempts);
+      } else {
+        rethrow;
+      }
+    }
   }
 }
